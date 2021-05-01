7e04295f2e845d4666fda5f9a8cacc742af4a075
Michael Bui <michaelbui99@hotmail.com>
2021-05-01 21:02:44 +0200

    Added search methods in dao

--- src/Test.java
+++ src/Test.java
@@ -1,9 +1,16 @@
+import client.model.material.Material;
+import client.model.material.reading.Book;
+import database.MaterialDAO;
+import database.MaterialDAOImpl;
 import server.model.LibraryModel;
 import server.model.LibraryModelManager;
 import server.network.RMIServerImpl;
 import shared.RMIServer;
 
 import java.rmi.RemoteException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Test
 {
@@ -11,10 +18,19 @@ public class Test
   {
     LibraryModel model = new LibraryModelManager();
     RMIServer rmiServer = new RMIServerImpl(model);
-    rmiServer
-        .registerBook("as", "ddee", "2020-01-01", "dreewqt", "ddbh", "Voksen", "Dansk", "12345", 500, 1, 1, "nothing", null);
-
-      rmiServer.createBookCopy(1);
-  //  rmiServer.registerLoan(model.searchMaterial("1"), "111111-1111", "1999-12-12");
+//    rmiServer
+//        .registerBook("as", "ddee", "2020-01-01", "dreewqt", "ddbh", "Voksen", "Dansk", "12345", 500, 1, 1, "nothing", null);
+//
+//      rmiServer.createBookCopy(1);
+    try
+    {
+      List<Material> materials= MaterialDAOImpl.getInstance().getAllMaterialByTitle("Title1");
+      System.out.println(materials);
+    }
+    catch (SQLException throwables)
+    {
+      throwables.printStackTrace();
+    }
+    //  rmiServer.registerLoan(model.searchMaterial("1"), "111111-1111", "1999-12-12");
   }
 }
