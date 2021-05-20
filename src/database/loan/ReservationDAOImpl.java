e93444cab2b02b76c45fba5a225eb979136b4285
Michael Bui <michaelbui99@hotmail.com>
2021-05-20 10:36:48 +0200

    Loan and Reservation bug fixes

--- src/database/loan/ReservationDAOImpl.java
+++ src/database/loan/ReservationDAOImpl.java
@@ -34,17 +34,17 @@ public class ReservationDAOImpl extends BaseDAO implements ReservationDAO
       try(Connection connection = getConnection())
       {
         //todo: lige nu reserverer man en specific kopy. Evt. tilføje materiale + materiale kopi som java object, så man skelner mellem dem?
-        //todo:
+        //todo: lav et check på om der allerede findes en reservation borrower CPR og MaterialID
         LocalDate today = LocalDate.now();
         PreparedStatement stm = connection.prepareStatement("INSERT INTO reservation (material_id, cpr_no, reservation_date, ready) values (?,?,?,false)", Statement.RETURN_GENERATED_KEYS);
         stm.setInt(1, material.getMaterialID());
         stm.setString(2, borrower.getCpr());
         stm.setDate(3,Date.valueOf(today));
         stm.executeUpdate();
+        connection.commit();
         ResultSet keys = stm.getGeneratedKeys();
         keys.next();
-        connection.commit();
-        return new Reservation(material, borrower, today, keys.getInt(1), false);
+        return new Reservation(material, borrower, today, keys.getInt("reservation_id"), false);
       }
     }
     catch (SQLException throwables)
